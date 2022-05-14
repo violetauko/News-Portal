09f79a21260db6d26b4d0312d2146f7e9b4d65d9
violetauko <aukoviolet24@gmail.com>
2022-05-15 00:48:58 +0300

    create sql2o dao classes

--- src/main/java/dao/Sql2oDepartmentDao.java
+++ src/main/java/dao/Sql2oDepartmentDao.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class Sql2oDepartmentDao implements DepartmentDao {
 
+
     private final Sql2o sql2o;
     public Sql2oDepartmentDao(Sql2o sql2o){
         this.sql2o = sql2o;
--- src/test/java/dao/Sql2oDepartmentDaoTest.java
+++ src/test/java/dao/Sql2oDepartmentDaoTest.java
@@ -3,17 +3,31 @@ package dao;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Sql2oDepartmentDaoTest {
+    private Connection conn;
+    private Sql2oDepartmentDao departmentDao;
+    private Sql2oNewsDao newsDao;
+    private Sql2oUsersDao usersDao;
+
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception{
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/tables.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        usersDao = new Sql2oUsersDao(sql2o);
+        conn = sql2o.open();
     }
 
     @AfterEach
-    void tearDown() {
+    void tearDown() throws Exception{
+        conn.close();
     }
 
     @Test
