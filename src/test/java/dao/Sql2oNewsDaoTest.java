66f16cec43ce00ab5aa3f164f45836637488ba7f
violetauko <aukoviolet24@gmail.com>
2022-05-15 01:34:32 +0300

    add more tests to news class

--- src/test/java/dao/Sql2oNewsDaoTest.java
+++ src/test/java/dao/Sql2oNewsDaoTest.java
@@ -1,27 +1,50 @@
 package dao;
 
+import models.News;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Sql2oNewsDaoTest {
+    private Sql2oNewsDao newsDao;
+    private Sql2oDepartmentDao departmentDao;
+    private Connection conn;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception{
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/tables.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        conn = sql2o.open();
     }
 
     @AfterEach
-    void tearDown() {
+    void tearDown() throws Exception {
+        conn.close();
     }
 
     @Test
-    void add() {
+    void add() throws Exception{
+        News testNews = setupNewNews();
+        int id = testNews.getId();
+        newsDao.add(testNews);
+        assertNotEquals(id, testNews.getId());
+
     }
 
     @Test
-    void getAll() {
+    void getAll() throws Exception{
+        News testNews = setupNewNews();
+        newsDao.add(testNews);
+        assertEquals(1,newsDao.getAll().size());
+
     }
 
     @Test
@@ -31,4 +54,9 @@ class Sql2oNewsDaoTest {
     @Test
     void clearAll() {
     }
+    public News setupNewNews(){
+        return new News("All employees should report to work on sunday",1);
+
+    }
+
 }
