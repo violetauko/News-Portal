10cfe74681006e823481f611d105d6aca6af302e
violetauko <aukoviolet24@gmail.com>
2022-05-15 22:40:42 +0300

    add connection on routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,25 @@
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUsersDao;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 public class App {
+    public static void main(String[] args) {
+        Sql2oUsersDao usersDao;
+        Sql2oDepartmentDao departmentDao;
+        Sql2oNewsDao newsDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:h2:~/jadle.db;INIT=RUNSCRIPT from 'classpath:db/tables.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        usersDao = new Sql2oUsersDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        conn = sql2o.open();
+
+    }
 }
